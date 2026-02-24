6e921ff769721f9d091167423f442b53e8509617
Aditya <adigarg838@gmail.com>
2026-02-24 10:29:31 +0530

    second commit loops 24/02/26

---
+++ loop.js
@@ -0,0 +1,7 @@
+var cout;
+document.write("starting loop" + "<br />");
+for (count = 0; count < 10; count++) {
+    document.write("Current count: " + count);
+    document.write("<br />");
+}
+document.write("loop stopped!");

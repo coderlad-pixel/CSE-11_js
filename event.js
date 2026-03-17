efd8d3df51c2ecd1730e7c92d5c0bd7fa5c50470
Aditya <adigarg838@gmail.com>
2026-03-17 10:02:56 +0530

    17/03/26 events and set attribute topic

---
+++ event.js
@@ -0,0 +1,21 @@
+function onClick() {
+    console.log("clicked");
+}
+function onMouseOver() {
+    console.log("on mouse over");
+}
+function onMouseOut() {
+    console.log("On mouse out");
+}
+function onMouseDown() {
+    console.log("on mouse down");
+}
+function onMouseUp() {
+    console.log("on mouse up");
+}
+function onMouseMove() {
+    console.log("on mouse move");
+}
+function onLoad() {
+    alert("something");
+}
---
+++ scripttt.js
@@ -0,0 +1,4 @@
+function myFunction() {
+    document.getElementById("demo").setAttribute("demo", "democlass");
+    console.log("hello")
+}
